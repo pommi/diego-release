aebff59e68aad4be195a512e4b67fdf4a900dde7
Sunjay Bhatia <sbhatia@pivotal.io>
2019-07-11 16:08:27 -0400

    WIP fix envoy blob path
    Co-authored-by: Edwin Xie <exie@pivotal.io>

--- scripts/ci/setup_inigo.ps1
+++ scripts/ci/setup_inigo.ps1
@@ -144,7 +144,7 @@ function Setup-Envoy() {
     $env:ENVOY_PATH="$env:TEMP\envoy"
     mkdir -Force "$env:ENVOY_PATH"
 
-    Expand-Archive -Force -Path "$mingwPath" -DestinationPath "$env:ENVOY_PATH"
+    Expand-Archive -Force -Path "blobs\envoy-nginx\envoy-nginx.zip" -DestinationPath "$env:ENVOY_PATH"
 
     $env:GOPATH="$PWD"
     go build -o "$env:ENVOY_PATH\envoy.exe" "code.cloudfoundry.org/envoy-nginx"
